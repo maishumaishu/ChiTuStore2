86447ab9c0885074fa3eb132193263f48f7cd006
maishu <ansiboy@163.com>
2016-11-20 08:40:19 +0800

    改进 VUE 定义文件

--- src/js/typings/vue.d.ts
+++ src/js/typings/vue.d.ts
@@ -1,12 +1,15 @@
-// declare cl Vue(options: {
-//     el: string,
-//     data: any
-// });
 
-declare class Vue {
-    constructor(options: { el: string | HTMLElement, data: any, methods?: any });
+declare interface VueInstance {
     $el: HTMLElement;
+    $watch(expOrFn: string | Function, callback: Function, options?: { deep?: boolean, immediate?: boolean }): Function;
+    $set(object: Object, key: string, value: any)
 }
+
+declare interface VueConstructor {
+    new <T>(options: { el: string | HTMLElement, data: T, methods?: any }): T & VueInstance;
+
+}
+declare let Vue: VueConstructor
 
 declare module 'vue' {
     export = Vue;
