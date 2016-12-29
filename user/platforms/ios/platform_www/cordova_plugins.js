9034b561e57b86b456220f7bc97870ebd5b988b8
maishu <ansiboy@163.com>
2016-12-29 12:54:31 +0800

    添加 ios 项目

---
+++ user/platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,10 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-crosswalk-webview": "2.2.0",
+    "cordova-plugin-whitelist": "1.3.1"
+};
+// BOTTOM OF METADATA
+});
