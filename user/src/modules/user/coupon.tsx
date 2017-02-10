c79984fd8f348d8bca568b7179d34aad0de2cad8
ansiboy <ansiboy@163.com>
2017-02-10 18:06:57 +0800

    加入新页面

---
+++ user/src/modules/user/coupon.tsx
@@ -0,0 +1,22 @@
+import { Page } from 'site';
+import { defaultNavBar } from 'site';
+let { PageComponent, PageHeader, PageFooter, PageView, ImageBox, DataList } = controls;
+
+export default function (page: Page) {
+    class CouponPage extends React.Component<{}, {}>{
+        render() {
+            return (
+                <PageComponent>
+                    <PageHeader>
+                        {defaultNavBar({ title: '我的优惠券' })}
+                    </PageHeader>
+                    <PageView>
+                        <h3 style={{ textAlign: 'center' }}>TODO</h3>
+                    </PageView>
+                </PageComponent>
+            )
+        }
+    }
+
+    ReactDOM.render(<CouponPage />, page.element);
+}
