3d9889632beb8bd65b1387656043fb88214a108b
ansiboy <ansiboy@163.com>
2017-02-15 22:38:25 +0800

    添加假数据

--- user/src/modules/user/messages.tsx
+++ user/src/modules/user/messages.tsx
@@ -5,13 +5,35 @@ let { PageComponent, PageHeader, PageFooter, PageView, ImageBox, DataList } = co
 export default function (page: Page) {
     class MessagePage extends React.Component<{}, {}>{
         render() {
+            let messags = [
+                { icon: '', color: '', title: '优化促销', text: '2.20　购买禾盒果买２送一', }
+            ]
             return (
                 <PageComponent>
                     <PageHeader>
                         {defaultNavBar({ title: '我的消息' })}
                     </PageHeader>
                     <PageView>
-                        <h3 style={{ textAlign: 'center' }}>TODO</h3>
+
+                        <ul className="list-group">
+                            {messags.map(o =>
+                                <li className="list-group-item">
+                                    <div className="pull-left" style={{ width: 40, height: 40, backgroundColor: 'red', textAlign: 'center' }}>
+                                        <i className="icon-thumbs-up" style={{ fontSize: 20, marginTop: 10, color: '#fff', display: 'block' }} />
+                                    </div>
+                                    <div style={{ textAlign: 'left', marginLeft: 50, }}>
+                                        <div>
+                                            {o.title}
+                                        </div>
+                                        <div>
+                                            {o.text}
+                                        </div>
+                                    </div>
+                                </li>
+                            )}
+                        </ul>
+                        <h3 style={{ textAlign: 'center' }}>
+                        </h3>
                     </PageView>
                 </PageComponent>
             )
