5a6a4fd3c732659bd2bb00747b0372d5619bce9e
ansiboy <ansiboy@163.com>
2017-04-12 14:44:04 +0800

    安全账户页面

--- user/src/modules/user/accountSecurity/index.tsx
+++ user/src/modules/user/accountSecurity/index.tsx
@@ -11,7 +11,32 @@ export default function (page: Page) {
                         {defaultNavBar({ title: '账户安全' })}
                     </PageHeader>
                     <PageView>
-                        <h3 style={{ textAlign: 'center' }}>TODO</h3>
+                        <div className="container">
+                            <div className="list-group">
+                                <a data-bind="attr:{href:mobileBindingUrl}" href="#AccountSecurity_Setting_MobileBinding" className="list-group-item row">
+                                    <strong className="name">手机绑定</strong>
+                                    <i className="icon-chevron-right pull-right"></i>
+                                    <span className="pull-right text-primary" style={{ paddingRight: 10 }}>
+                                        未设置
+                                    </span>
+                                    <div style={{ paddingTop: 10 }}>绑定手机后，你可以通过手机找回密码</div>
+                                </a>
+                                <a href="#AccountSecurity_Setting_LoginPassword" className="list-group-item row">
+                                    <strong className="name">登录密码</strong>
+                                    <i className="icon-chevron-right pull-right"></i>
+                                    <span data-bind="visible:!passwordSetted()" className="pull-right text-primary" style={{ paddingRight: 10 }}>未设置</span>
+                                    <div style={{ paddingTop: 10 }}>设置登录密码，可以使用手机和密码登录</div>
+                                </a>
+                                <a href="#AccountSecurity_Setting_PaymentPassword" className="list-group-item row">
+                                    <strong className="name">支付密码</strong>
+                                    <i className="icon-chevron-right pull-right"></i>
+                                    <span data-bind="visible:!paymentPasswordSetted()" className="pull-right text-primary" style={{ paddingRight: 10 }}>未设置</span>
+                                    <div style={{ paddingTop: 10 }}>设置支付密码后，使用余额支付需要密码</div>
+                                </a>
+                            </div>
+                            <div className="list-group">
+                            </div>
+                        </div>
                     </PageView>
                 </PageComponent>
             )
