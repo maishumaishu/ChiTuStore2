c25e937a8b089cf863dd71aa51500e9749c03c1b
ansiboy <ansiboy@163.com>
2017-04-25 11:55:03 +0800

    余额支付

--- user/src/modules/shopping/purchase.tsx
+++ user/src/modules/shopping/purchase.tsx
@@ -1,29 +1,40 @@
 import { Page, defaultNavBar, app, formatDate } from 'site';
 import { ShoppingService, AccountService, Order } from 'services';
+import * as ui from 'ui';
 
 let { PageComponent, PageHeader, PageFooter, PageView, Dialog, Button } = controls;
 
 export default function (page: Page) {
 
     let shopping = page.createService(ShoppingService);
-
-    class PurchasePage extends React.Component<{ order: Order }, {}> {
-        private purchase() {
-            return Promise.resolve();
+    let accout = page.createService(AccountService);
+    type PayType = 'balance' | 'weixin' | 'alipay';
+    class PurchasePage extends React.Component<{ order: Order }, { payType: PayType }> {
+        constructor(props) {
+            super(props);
+            this.state = { payType: 'balance' };
+        }
+        private balancePurchase(order: Order) {
+            console.assert(order.Sum != null);
+            console.assert(order.Sum == order.Amount + order.Freight);
+            return accout.payOrder(order.Id, order.Sum);
         }
         render() {
             let order = this.props.order;
             return (
                 <PageComponent>
                     <PageHeader>
-                        {defaultNavBar({ title: '订单概况' })}
+                        {defaultNavBar({
+                            title: '订单概况',
+                            back: () => {
+                                if (page.previous.name == 'shopping.orderList') {
+                                    app.back();
+                                    return;
+                                }
+                                app.redirect('shopping_orderList');
+                            }
+                        })}
                     </PageHeader>
-                    <PageFooter>
-                        {order.Status == 'WaitingForPayment' ?
-                            <div className="container">
-                                <Button onClick={() => this.purchase()} className="btn btn-block btn-primary">微信支付</Button>
-                            </div> : null}
-                    </PageFooter>
                     <PageView>
                         <div className="container">
                             <div className="row" style={{ paddingBottom: 10 }}>
@@ -38,7 +49,7 @@ export default function (page: Page) {
                             </div>
                             <div className="row" style={{ paddingBottom: 10 }}>
                                 <label className="col-xs-3" style={{ paddingRight: 0 }}>订单总计</label>
-                                <div className="col-xs-9 price">￥{order.Sum}</div>
+                                <div className="col-xs-9 price">￥{order.Sum.toFixed(2)}</div>
                             </div>
                             <div className="row" style={{ paddingBottom: 10 }}>
                                 <label className="col-xs-3" style={{ paddingRight: 0 }}>收货信息</label>
@@ -64,9 +75,29 @@ export default function (page: Page) {
                             <div style={{ marginBottom: 10 }}>
                                 提示：请在下单24小时内付款，过期后订单将自动取消。
                             </div>
-
+                            <hr className="row" />
+                            <button className="cust-prop selected">
+                                余额支付
+                            </button>
+                            <button className="cust-prop">
+                                微信支付
+                            </button>
                         </div>
                     </PageView>
+                    <PageFooter>
+                        {order.Status == 'WaitingForPayment' ?
+                            <div className="container">
+                                <button className="btn btn-block btn-primary"
+                                    ref={(o: HTMLButtonElement) => {
+                                        if (!o) return;
+                                        o.onclick = ui.buttonOnClick(() => {
+                                            return this.balancePurchase(order).then(() => {
+                                                app.redirect('shopping_orderList');
+                                            });
+                                        })
+                                    }}>立即支付</button>
+                            </div> : null}
+                    </PageFooter>
                 </PageComponent >
             );
         }
